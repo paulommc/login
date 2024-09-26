cfc36422bdff7e44d773bd7d06977d20c86d1d6d
paulommc <paulommc@gmail.com>
2024-09-25 23:29:08 -0300

    Adicionado "Seu carrinho está vazio" quando não houver itens no carriho

--- lib/view/carrinho_view.dart
+++ lib/view/carrinho_view.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import '../model/carrinho.dart';
@@ -17,120 +17,166 @@ class _CarrinhoViewState extends State<CarrinhoView> {
   Widget build(BuildContext context) {
     // Calcula a soma total dos itens
     double total = itens.fold(0, (soma, item) => soma + item['valor']);
-
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          leading: InkWell(
-            onTap: () {
-              Navigator.pop(context);
-            },
-            child: Icon(
-              Icons.arrow_back_ios,
-              color: Colors.orange,
-            ),
-          ),
-          title: Text('Seu Pedido'),
-          centerTitle: true,
-          actions: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(right: 10),
-              child: IconButton(
-                icon: const Icon(Icons.logout),
+    if (itens.isEmpty) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            leading: InkWell(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Icon(
+                Icons.arrow_back_ios,
                 color: Colors.orange,
-                tooltip: 'Sair',
-                onPressed: () {
-                  Navigator.pushNamedAndRemoveUntil(
-                      context, 'login', (Route<dynamic> route) => false);
-                },
               ),
             ),
-          ],
-        ),
-        body: Column(
-          children: [
-            // Lista de produtos
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.all(10),
-                child: ListView.builder(
-                  itemCount: itens.length,
-                  itemBuilder: (context, index) {
-                    return Card(
-                      child: ListTile(
-                        title: Text(
-                          itens[index]['nome'],
-                          style: TextStyle(fontSize: 24),
-                        ),
-                        subtitle: Text(
-                          "R\$ ${itens[index]['valor'].toStringAsFixed(2).replaceAll('.', ',')}",
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontStyle: FontStyle.italic,
-                          ),
-                        ),
-                        leading: Container(
-                          width: 50,
-                          height: 50,
-                          child: Image.asset(
-                            itens[index]['imagem'],
-                            width: 50,
-                            height: 50,
-                          ),
-                        ),
-                        trailing: InkWell(
-                          onTap: () {
-                            setState(() {
-                              itens.removeAt(index);
-                            });
-                          },
-                          child: Icon(
-                            Icons.cancel_outlined,
-                            size: 30,
-                            color: Colors.red.shade900,
-                          ),
-                        ),
-                      ),
-                    );
+            title: Text('Seu Pedido'),
+            centerTitle: true,
+            actions: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(right: 10),
+                child: IconButton(
+                  icon: const Icon(Icons.logout),
+                  color: Colors.orange,
+                  tooltip: 'Sair',
+                  onPressed: () {
+                    Navigator.pushNamedAndRemoveUntil(
+                        context, 'login', (Route<dynamic> route) => false);
                   },
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Text(
-                'Total: R\$ ${total.toStringAsFixed(2).replaceAll('.', ',')}',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.orange,
+            ],
+          ),
+          body: Column(
+            children: [
+              SizedBox(height: 300),
+              Center(
+                child: Text(
+                  'Seu carrinho está vazio.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 20, color: Colors.grey.shade600),
                 ),
               ),
+            ],
+          ),
+        ),
+      );
+    } else {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            leading: InkWell(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Icon(
+                Icons.arrow_back_ios,
+                color: Colors.orange,
+              ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.orange,
+            title: Text('Seu Pedido'),
+            centerTitle: true,
+            actions: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(right: 10),
+                child: IconButton(
+                  icon: const Icon(Icons.logout),
+                  color: Colors.orange,
+                  tooltip: 'Sair',
+                  onPressed: () {
+                    Navigator.pushNamedAndRemoveUntil(
+                        context, 'login', (Route<dynamic> route) => false);
+                  },
                 ),
-                onPressed: () {},
+              ),
+            ],
+          ),
+          body: Column(
+            children: [
+              // Lista de produtos
+              Expanded(
                 child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 15.0, horizontal: 60.0),
-                  child: Text(
-                    'Finalizar Pedido',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
+                  padding: EdgeInsets.all(10),
+                  child: ListView.builder(
+                    itemCount: itens.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: ListTile(
+                          title: Text(
+                            itens[index]['nome'],
+                            style: TextStyle(fontSize: 24),
+                          ),
+                          subtitle: Text(
+                            "R\$ ${itens[index]['valor'].toStringAsFixed(2).replaceAll('.', ',')}",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontStyle: FontStyle.italic,
+                            ),
+                          ),
+                          leading: Container(
+                            width: 50,
+                            height: 50,
+                            child: Image.asset(
+                              itens[index]['imagem'],
+                              width: 50,
+                              height: 50,
+                            ),
+                          ),
+                          trailing: InkWell(
+                            onTap: () {
+                              setState(() {
+                                itens.removeAt(index);
+                              });
+                            },
+                            child: Icon(
+                              Icons.cancel_outlined,
+                              size: 30,
+                              color: Colors.red.shade900,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  'Total: R\$ ${total.toStringAsFixed(2).replaceAll('.', ',')}',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.orange,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.orange,
+                  ),
+                  onPressed: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 15.0, horizontal: 60.0),
+                    child: Text(
+                      'Finalizar Pedido',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    }
   }
 }
